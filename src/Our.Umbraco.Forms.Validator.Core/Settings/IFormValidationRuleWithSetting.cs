fcf1341e841f89582c5c40a9964ff99b255683c7
Luke Fisher <mail@lukefisher.me>
2023-02-23 18:32:53 +0000

    Introduce combined rule with setting interface

---
+++ src/Our.Umbraco.Forms.Validator.Core/Settings/IFormValidationRuleWithSetting.cs
@@ -0,0 +1,13 @@
+// Copyright 2023 Luke Fisher
+// SPDX-License-Identifier: Apache-2.0
+
+using Our.Umbraco.Forms.Validator.Core.Rules;
+
+namespace Our.Umbraco.Forms.Validator.Core.Settings;
+
+public interface IFormValidationRuleWithSetting
+{
+    IFormValidationRule Rule { get; }
+
+    IFormValidationRuleSetting Setting { get; }
+}
