6c2370e66ba7e4bb55e54cb8cb9df367dbca523f
Luke Fisher <mail@lukefisher.me>
2023-05-13 21:09:22 +0100

    Add in mapper on startup

--- src/Our.Umbraco.Forms.Validator/Composers/PluginComposer.cs
+++ src/Our.Umbraco.Forms.Validator/Composers/PluginComposer.cs
@@ -2,19 +2,24 @@
 // SPDX-License-Identifier: Apache-2.0
 
 using Our.Umbraco.Forms.Validator.Backoffice;
+using Our.Umbraco.Forms.Validator.Persistence.Mappers;
 using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Extensions;
 
 namespace Our.Umbraco.Forms.Validator.Composers;
 
-public class PluginComposer: IComposer
+public class PluginComposer : IComposer
 {
     public void Compose(IUmbracoBuilder builder)
     {
         builder.ContentApps()
             .Append<SettingContentApp>();
-        
+
         builder.ManifestFilters()
             .Append<PluginManifestFilter>();
+
+        builder.Mappers()?
+            .Add<FormValidationSettingMapper>();
     }
 }
